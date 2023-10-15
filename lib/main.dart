fa4cc453bab052e765095c7ebb92837ed26d2fa4
James Tennant <jtennant@ucsc.edu>
2023-10-15 14:37:06 -0700

    About screen (#4)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ void main() {
   runApp(const App());
 }
 
+var routes = {
+  '/': (context) => const HomeScreen(),
+  '/about': (context) => const AboutScreen(),
+};
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -11,11 +16,34 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello, World!'),
+      routes: routes,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FilledButton(
+          onPressed: () => Navigator.pushNamed(context, '/about'),
+          child: const Text('About'),
         ),
       ),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('About')),
     );
   }
 }
