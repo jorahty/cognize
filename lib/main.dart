a1e82684be5ff5cdeff77f668a5d0339cb841fc5
James Tennant <jtennant@ucsc.edu>
2023-10-16 17:23:20 -0700

    Setup Firebase (#6)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:cognize/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
