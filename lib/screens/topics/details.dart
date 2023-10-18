dada4f906b2ada31bc452cc2473d3f1e63385e85
James Tennant <jtennant@ucsc.edu>
2023-10-17 23:20:34 -0700

    Make topic details screen more responsive

--- lib/screens/topics/details.dart
+++ lib/screens/topics/details.dart
@@ -13,33 +13,30 @@ class TopicDetails extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
-      body: ListView(
-        children: [
-          LayoutBuilder(
-            builder: (context, constraints) {
-              if (constraints.maxWidth < 900) {
-                return Image.asset(
-                  'assets/placeholder.png',
-                  width: MediaQuery.of(context).size.width,
-                );
-              } else {
-                return Container();
-              }
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              topic.title,
-              style: GoogleFonts.inter(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+      body: Center(
+        child: SizedBox(
+          width: 700,
+          child: ListView(
+            children: [
+              Image.asset(
+                'assets/placeholder.png',
+                width: MediaQuery.of(context).size.width,
               ),
-              overflow: TextOverflow.fade,
-              softWrap: false,
-            ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  topic.title,
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
