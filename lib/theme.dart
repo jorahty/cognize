a02c02f2b71a45a60a7a58571d2aac0772d7537b
James Tennant <jtennant@ucsc.edu>
2023-10-17 21:48:04 -0700

    Left-align the app bar titles on all platforms (#31)

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,4 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 final theme = ThemeData(
   fontFamily: GoogleFonts.inter().fontFamily,
   brightness: Brightness.dark,
+  appBarTheme: const AppBarTheme(
+    centerTitle: false,
+  ),
 );
