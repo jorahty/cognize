710f71f5619c16e70df3a6787c011a85b2118d94
James Tennant <jtennant@ucsc.edu>
2023-10-19 21:48:21 -0700

    Fetch quiz data from Firestore

--- lib/screens/quiz/screen.dart
+++ lib/screens/quiz/screen.dart
@@ -1,4 +1,6 @@
+import 'package:cognize/services/firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cognize/services/models.dart';
 
 class QuizScreen extends StatelessWidget {
   final String quizId;
@@ -6,8 +8,20 @@ class QuizScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return FutureBuilder<Quiz>(
+      future: FirestoreService().getQuiz(quizId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          final quiz = snapshot.data ?? Quiz();
+
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(child: Text(quiz.title)),
+          );
+        }
+      },
     );
   }
 }
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -13,4 +13,10 @@ class FirestoreService {
     var topics = data.map((d) => Topic.fromJson(d));
     return topics.toList();
   }
+
+  Future<Quiz> getQuiz(String quizId) async {
+    var ref = _db.collection('quizzes').doc(quizId);
+    var snapshot = await ref.get();
+    return Quiz.fromJson(snapshot.data()!);
+  }
 }
