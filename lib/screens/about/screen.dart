a68d8aa4a2adfb5cfd15ba71c8d29ea954d34a11
JKIO612 <38957018+JKIO612@users.noreply.github.com>
2023-10-21 12:52:24 -0700

    about screen for now

--- lib/screens/about/screen.dart
+++ lib/screens/about/screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AboutScreen extends StatelessWidget {
-  const AboutScreen({super.key});
+  const AboutScreen({Key? key}) : super(key: key); // fixed key initialization
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +12,22 @@ class AboutScreen extends StatelessWidget {
           icon: const Icon(FontAwesomeIcons.chevronLeft),
           onPressed: () => Navigator.of(context).pop(),
           tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+        ),
+      ),
+      body: Center(
+        // Added Center widget to center its children
+        child: Padding(
+          // Added Padding for some spacing
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            "Cognize is an app that allows you to take quizzes with ease, it's the next quizlet!",
+            textAlign: TextAlign.center, // To align text to center
+            style: TextStyle(
+              // Optional: for styling your text
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
       ),
     );
