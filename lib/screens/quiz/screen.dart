238c0d937698c5febb55f4589e13de52881ef08c
James Tennant <jtennant@ucsc.edu>
2023-10-20 19:00:02 -0700

    quiz progress bar

--- lib/screens/quiz/screen.dart
+++ lib/screens/quiz/screen.dart
@@ -4,24 +4,32 @@ import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:cognize/services/firestore.dart';
 import 'package:cognize/services/models.dart';
+import 'package:cognize/widgets/common/progress_bar.dart';
 
 class QuizState with ChangeNotifier {
   final PageController controller = PageController();
+
+  Option? _selectedOption;
+  double _quizProgress = 0;
+
+  Option? get selectedOption => _selectedOption;
+  double get quizProgress => _quizProgress;
+
+  set selectedOption(Option? option) {
+    _selectedOption = option;
+    notifyListeners();
+  }
+
+  set quizProgress(double value) {
+    _quizProgress = value;
+    notifyListeners();
+  }
 
   nextPage() async {
     await controller.nextPage(
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeOutQuart,
     );
-  }
-
-  Option? _selectedOption;
-
-  Option? get selectedOption => _selectedOption;
-
-  set selectedOption(Option? option) {
-    _selectedOption = option;
-    notifyListeners();
   }
 }
 
@@ -50,6 +58,7 @@ class QuizScreen extends StatelessWidget {
                 onPressed: () => Navigator.pop(context),
                 tooltip: 'Exit quiz',
               ),
+              title: AnimatedProgressBar(value: state.quizProgress),
             ),
             body: Center(
               child: SizedBox(
@@ -58,6 +67,8 @@ class QuizScreen extends StatelessWidget {
                   scrollDirection: Axis.vertical,
                   physics: const NeverScrollableScrollPhysics(),
                   controller: state.controller,
+                  onPageChanged: (index) => state.quizProgress =
+                      (index / (quiz.questions.length + 1)),
                   itemCount: quiz.questions.length + 2,
                   itemBuilder: (context, index) {
                     late final Widget page;
