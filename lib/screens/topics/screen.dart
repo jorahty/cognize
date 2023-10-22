81c9ae18da88f82b966d68f079647135ad2cf3b9
Joel Sojan <joelsojan2002@gmail.com>
2023-10-21 22:37:29 -0700

    Added user profile icon at top right of page.

--- lib/screens/topics/screen.dart
+++ lib/screens/topics/screen.dart
@@ -4,12 +4,15 @@ import 'package:cognize/services/models.dart';
 import 'package:cognize/services/firestore.dart';
 import 'package:cognize/widgets/topic/grid.dart';
 import 'package:cognize/screens/topics/drawer.dart';
+import 'package:cognize/services/auth.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = AuthService().user!;
+
     return FutureBuilder<List<Topic>>(
       future: FirestoreService().getTopics(), // Fetch topics
       builder: (context, snapshot) {
@@ -20,12 +23,29 @@ class TopicsScreen extends StatelessWidget {
           return Scaffold(
             appBar: AppBar(
               title: const Text('Cognize'),
-              leading: Builder(
-                builder: (BuildContext context) => IconButton(
-                  icon: const Icon(FontAwesomeIcons.bars),
-                  onPressed: () => Scaffold.of(context).openDrawer(),
-                ),
+              leading: Row(
+                children: [
+                  Builder (
+                    builder: (BuildContext context) => IconButton(
+                      icon: const Icon(FontAwesomeIcons.bars),
+                      onPressed: () => Scaffold.of(context).openDrawer(),
+                    )
+                  ),
+                ],
               ),
+              actions: [
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(user.photoURL!),
+                      ),
+                    )
+                  ],
+                )
+              ]
             ),
             drawer: TopicsDrawer(topics: topics),
             body: _Body(topics: topics),
