e07c35f12bd25f02499aeea9db79da010b0a7bac
JKIO612 <38957018+JKIO612@users.noreply.github.com>
2023-10-21 23:07:48 -0700

    freshening up

--- lib/screens/about/screen.dart
+++ lib/screens/about/screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AboutScreen extends StatelessWidget {
-  const AboutScreen({Key? key}) : super(key: key); // fixed key initialization
+  const AboutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +15,48 @@ class AboutScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        // Added Center widget to center its children
-        child: Padding(
-          // Added Padding for some spacing
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            "Cognize is an app that allows you to take quizzes with ease, it's the next quizlet!",
-            textAlign: TextAlign.center, // To align text to center
-            style: TextStyle(
-              // Optional: for styling your text
-              fontSize: 18.0,
-              fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Card(
+            elevation: 8.0,
+            margin: const EdgeInsets.all(16.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Cognize is a free quiz app that allows self learning through quizzes on multiple subjects on this platform.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white54,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "It allows for users to learn from predefined quizzes or collaborate and learn from other peoples quizzes or self-created quizzes.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "It allows for the ability to learn from any device with integrated support for Windows, IOS and Android devices.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
