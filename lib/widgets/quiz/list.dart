ef20819801b8d8d330c939bf75f01fa975338f1b
James Tennant <jtennant@ucsc.edu>
2023-10-24 03:24:02 -0700

    animate topic and quiz list

--- lib/widgets/quiz/list.dart
+++ lib/widgets/quiz/list.dart
@@ -1,6 +1,7 @@
 import 'package:cognize/services/models.dart';
 import 'package:cognize/widgets/quiz/card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class QuizList extends StatelessWidget {
   const QuizList({super.key, required this.topic});
@@ -11,7 +12,10 @@ class QuizList extends StatelessWidget {
     return Column(
       children: topic.quizzes
           .map((quiz) => QuizCard(topic: topic, quiz: quiz))
-          .toList(),
+          .toList()
+          .animate(interval: 80.ms)
+          .slide(duration: 400.ms, curve: const Cubic(0.5, 0.5, 0.7, 1.2))
+          .scaleXY(),
     );
   }
 }
--- lib/widgets/topic/grid.dart
+++ lib/widgets/topic/grid.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cognize/services/models.dart';
 import 'package:cognize/widgets/topic/card.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class TopicGrid extends StatelessWidget {
   final List<Topic> topics;
@@ -14,7 +15,12 @@ class TopicGrid extends StatelessWidget {
           10, 10, 10, MediaQuery.of(context).size.height * 0.7),
       mainAxisSpacing: 10,
       crossAxisSpacing: 10,
-      children: topics.map((topic) => TopicCard(topic: topic)).toList(),
+      children: topics
+          .map((topic) => TopicCard(topic: topic))
+          .toList()
+          .animate(interval: 80.ms)
+          .slide(duration: 500.ms, curve: const Cubic(0.5, 0.5, 0.7, 1.5))
+          .scaleXY(),
     );
   }
 }
