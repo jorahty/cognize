5172644c8ed8bc627130dd36f0bd7303119c6591
James Tennant <jtennant@ucsc.edu>
2023-10-23 18:56:30 -0700

    add logo to auth page

--- lib/pages/auth/page.dart
+++ lib/pages/auth/page.dart
@@ -11,14 +11,23 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Button(
-          onPressed: AuthService().googleSignIn,
-          leading: SvgPicture.asset(
-            'assets/google_logo.svg',
-            width: 18,
-            height: 18,
-          ),
-          label: const Text('Continue with Google'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SvgPicture.asset(
+              'assets/logo.svg',
+              width: 280,
+            ),
+            Button(
+              onPressed: AuthService().googleSignIn,
+              leading: SvgPicture.asset(
+                'assets/google_logo.svg',
+                width: 18,
+                height: 18,
+              ),
+              label: const Text('Continue with Google'),
+            ),
+          ],
         ),
       ),
     );
