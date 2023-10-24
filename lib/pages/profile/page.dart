a04e7c9c40a4b0685a06d22a2f0be45508d2629e
James Tennant <jtennant@ucsc.edu>
2023-10-23 18:12:25 -0700

    extract user details

--- lib/pages/profile/page.dart
+++ lib/pages/profile/page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../../services/auth.dart';
-import '../../services/models.dart';
-import '../../widgets/common/button.dart';
-import '../../widgets/auth_gate.dart';
+import 'package:cognize/widgets/user/details.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -23,62 +18,5 @@ class ProfilePage extends StatelessWidget {
       ),
       body: const UserDetails(),
     );
-  }
-}
-
-class UserDetails extends StatelessWidget {
-  const UserDetails({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final user = AuthService().user;
-    final report = Provider.of<Report>(context);
-
-    if (user == null) {
-      return const SizedBox.shrink();
-    } else {
-      return Container(
-        padding: const EdgeInsets.all(30),
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.network(user.photoURL!),
-            ),
-            Text(user.displayName!),
-            const Spacer(),
-            Text('${report.total}'),
-            const Text('Quizzes Completed'),
-            const Spacer(),
-            Button(
-              onPressed: () async {
-                await AuthService().signOut().then((_) {
-                  Navigator.of(context).pushAndRemoveUntil(
-                    PageRouteBuilder(
-                      pageBuilder: (BuildContext context,
-                          Animation<double> animation,
-                          Animation<double> secondaryAnimation) {
-                        return const AuthGate();
-                      },
-                      transitionsBuilder: (BuildContext context,
-                          Animation<double> animation,
-                          Animation<double> secondaryAnimation,
-                          Widget child) {
-                        return child; // No transition, just return the child
-                      },
-                    ),
-                    (route) => false,
-                  );
-                });
-              },
-              leading: const Icon(FontAwesomeIcons.arrowRightFromBracket),
-              label: const Text('Logout'),
-            ),
-            const Spacer(),
-          ],
-        ),
-      );
-    }
   }
 }
