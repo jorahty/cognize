a9e6605e0460a720a84fa6e2cec167159aab67b6
Abel-A <aasrese@ucsc.edu>
2023-11-21 20:01:09 -0800

    broken

--- lib/pages/create/quiz_questions_page.dart
+++ lib/pages/create/quiz_questions_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'quiz_submit_page.dart';
 import 'package:cognize/services/models.dart';
+import 'package:cognize/services/firestore.dart';
+
 import 'dart:convert';
 
 class QuizQuestionsPage extends StatefulWidget {
@@ -22,15 +24,17 @@ class QuizQuestionsPage extends StatefulWidget {
 class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
   late Quiz userCreatedQuiz;
   List<List<Option>> optionValues = [];
+  late String selectedTopicId; // New variable to store selected topic ID
 
   @override
   void initState() {
     super.initState();
+    selectedTopicId = ''; // Initialize the selectedTopicId
 
     userCreatedQuiz = Quiz(
       id: FirebaseFirestore.instance.collection('quizzes').doc().id,
       title: widget.title,
-      topic: widget.category,
+      topic: selectedTopicId, // Set topic to selectedTopicId
       description: 'Default Description',
       questions: List.generate(widget.numQuestions, (index) {
         optionValues.add(List.generate(3, (i) => Option(value: '', detail: '', correct: false)));
@@ -42,38 +46,20 @@ class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
     );
   }
 
-  void submitQuizToFirestore() async {
+  void submitTopicToFirestore(Topic topic) async {
     try {
-      List<Map<String, dynamic>> questionsData = [];
+      // Convert Topic to Map
+      Map<String, dynamic> topicData = topic.toMap();
 
-      for (int i = 0; i < userCreatedQuiz.questions.length; i++) {
-        Map<String, dynamic> questionData = {
-          'text': userCreatedQuiz.questions[i].text,
-          'options': optionValues[i].map((option) => option.toJson()).toList(),
-        };
-
-        questionsData.add(questionData);
-      }
-
-      Map<String, dynamic> quizData = {
-        'id': userCreatedQuiz.id,
-        'title': userCreatedQuiz.title,
-        'topic': userCreatedQuiz.topic,
-        'description': userCreatedQuiz.description,
-        'video': userCreatedQuiz.video,
-        'questions': questionsData,
-      };
-
-      print('QuizData before submission: $quizData');
-
+      // Submit Topic to Firestore
       await FirebaseFirestore.instance
-          .collection('quizzes')
-          .doc(userCreatedQuiz.id)
-          .set(quizData);
+          .collection('topics')
+          .doc(topic.id)
+          .set(topicData);
 
-      print('Quiz submitted to Firestore!');
+      print('Topic submitted to Firestore!');
     } catch (error) {
-      print('Error submitting quiz to Firestore: $error');
+      print('Error submitting topic to Firestore: $error');
     }
   }
 
@@ -116,7 +102,7 @@ class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
                       },
                     ),
                     SizedBox(height: 16),
-
+                    
                     for (int i = 0; i < 3; i++) ...[
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 8),
@@ -169,8 +155,30 @@ class _QuizQuestionsPageState extends State<QuizQuestionsPage> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ElevatedButton(
-          onPressed: () {
-            submitQuizToFirestore();
+          onPressed: () async {
+            // Fetch existing topics
+            List<Topic> existingTopics = await FirestoreService().getTopics();
+
+            // Find the selected topic
+            Topic? existingTopic = existingTopics.firstWhere(
+              (topic) => topic.id == selectedTopicId,
+              orElse: () => null,
+            );
+
+            if (existingTopic == null) {
+              print('Selected topic not found in existing topics.');
+              return;
+            }
+
+            // Update quizzes in the selected topic
+            List<Quiz> quizList = existingTopic.quizzes;
+            quizList.add(userCreatedQuiz);
+            existingTopic.quizzes = quizList;
+
+            // Submit the updated topic to Firestore
+            submitTopicToFirestore(existingTopic);
+
+            // Navigate to the next page
             Navigator.push(
               context,
               MaterialPageRoute(
