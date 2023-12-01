e9206c0e42f10886e5bec14c97d31a1ceac6027c
James Tennant <jtennant@ucsc.edu>
2023-11-30 18:03:08 -0800

    disable chatGPT for now ...

--- lib/widgets/helper.dart
+++ lib/widgets/helper.dart
@@ -12,25 +12,25 @@ class CognizeHelper extends StatefulWidget {
   State<CognizeHelper> createState() => _CognizeHelperState();
 }
 
-class _CognizeHelperState extends State<CognizeHelper>{
+class _CognizeHelperState extends State<CognizeHelper> {
   late final TextEditingController promptController;
-  String responseTxt = '';
+  String responseTxt = ''; // THIS IS THE RESPONSE
   late ResponseModel _responseModel;
 
   @override
-  void initState(){
+  void initState() {
     promptController = TextEditingController();
     super.initState();
   }
 
   @override
-  void dispose(){
+  void dispose() {
     promptController.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff343541),
       appBar: AppBar(
@@ -45,42 +45,44 @@ class _CognizeHelperState extends State<CognizeHelper>{
         children: [
           PromptBldr(responseTxt: responseTxt),
           TextFormFieldBldr(
-            promptController: promptController, btnFun: completionFun),
+              promptController: promptController, btnFun: completionFun),
         ],
       ),
     );
   }
 
-  //Handles http API call for ChatGPT
-  completionFun() async { 
-    setState(() => responseTxt = 'Loading...');
+  // Handles http API call for ChatGPT
+  completionFun() async {
+    setState(() => responseTxt = 'Not Implemented');
 
-    final response = await http.post(
-      Uri.parse('https://api.openai.com/v1/completions'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${dotenv.env['token']}'
-      },
-      body: jsonEncode(
-        {
-          "model": "text-davinci-003",
-          "prompt": promptController.text,
-          "max_tokens": 250,
-          "temperature": 0,
-          "top_p": 1,
-        },
-      ),
-    );
+    // Send POST request to openai.com ...
+    // final response = await http.post(
+    //   Uri.parse('https://api.openai.com/v1/completions'),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization':
+    //         'Bearer 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'
+    //   },
+    //   body: jsonEncode(
+    //     {
+    //       "model": "text-davinci-003",
+    //       "prompt": promptController.text,
+    //       "max_tokens": 250,
+    //       "temperature": 0,
+    //       "top_p": 1,
+    //     },
+    //   ),
+    // );
 
-    setState(() {
-      _responseModel = ResponseModel.fromJson(response.body);
-      responseTxt = _responseModel.choices[0]['text'];
-      debugPrint(responseTxt);
-    });
+    // setState(() {
+    //   _responseModel = ResponseModel.fromJson(response.body);
+    //   responseTxt = _responseModel.choices[0]['text'];
+    //   debugPrint(responseTxt);
+    // });
   }
 }
 
-class PromptBldr extends StatelessWidget{
+class PromptBldr extends StatelessWidget {
   const PromptBldr({
     super.key,
     required this.responseTxt,
@@ -89,7 +91,7 @@ class PromptBldr extends StatelessWidget{
   final String responseTxt;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height / 1.35,
       color: const Color(0xff434654),
@@ -110,15 +112,15 @@ class PromptBldr extends StatelessWidget{
   }
 }
 
-class TextFormFieldBldr extends StatelessWidget{
+class TextFormFieldBldr extends StatelessWidget {
   const TextFormFieldBldr(
-    {super.key, required this.promptController, required this.btnFun});
-  
+      {super.key, required this.promptController, required this.btnFun});
+
   final TextEditingController promptController;
   final Function btnFun;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Padding(
@@ -136,7 +138,7 @@ class TextFormFieldBldr extends StatelessWidget{
                     borderSide: const BorderSide(
                       color: Color(0xff444653),
                     ),
-                      borderRadius: BorderRadius.circular(5.5),
+                    borderRadius: BorderRadius.circular(5.5),
                   ),
                   enabledBorder: const OutlineInputBorder(
                     borderSide: BorderSide(
@@ -151,18 +153,17 @@ class TextFormFieldBldr extends StatelessWidget{
               ),
             ),
             Container(
-              color: const Color(0xff19bc99),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: IconButton(
-                  onPressed: () => btnFun(),
-                  icon: const Icon(
-                    Icons.send,
-                    color: Colors.white,
+                color: const Color(0xff19bc99),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: IconButton(
+                    onPressed: () => btnFun(),
+                    icon: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              )
-            )
+                ))
           ],
         ),
       ),
@@ -172,25 +173,25 @@ class TextFormFieldBldr extends StatelessWidget{
 
 // Defining the ResponseModel class
 class ResponseModel {
-final String id;
-final String object;
-final String model;
-final List choices;
+  final String id;
+  final String object;
+  final String model;
+  final List choices;
 
-ResponseModel(
-this.id,
-this.object,
-this.model,
-this.choices,
+  ResponseModel(
+    this.id,
+    this.object,
+    this.model,
+    this.choices,
   );
 
-ResponseModel copyWith({
-String? id,
-String? object,
-String? model,
-List? choices,
+  ResponseModel copyWith({
+    String? id,
+    String? object,
+    String? model,
+    List? choices,
   }) {
-return ResponseModel(
+    return ResponseModel(
       id ?? this.id,
       object ?? this.object,
       model ?? this.model,
@@ -198,48 +199,48 @@ return ResponseModel(
     );
   }
 
-Map<String, dynamic> toMap() {
-return <String, dynamic>{
-'id': id,
-'object': object,
-'model': model,
-'choices': choices,
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'object': object,
+      'model': model,
+      'choices': choices,
     };
   }
 
-factory ResponseModel.fromMap(Map<String, dynamic> map) {
-return ResponseModel(
+  factory ResponseModel.fromMap(Map<String, dynamic> map) {
+    return ResponseModel(
       map['id'] as String,
       map['object'] as String,
       map['model'] as String,
-List.from(
+      List.from(
         (map['choices'] as List),
       ),
     );
   }
 
-String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-factory ResponseModel.fromJson(String source) =>
-ResponseModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory ResponseModel.fromJson(String source) =>
+      ResponseModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
-@override
-String toString() {
-return 'ResponseModel(id: $id, object: $object, model: $model, choices: $choices)';
+  @override
+  String toString() {
+    return 'ResponseModel(id: $id, object: $object, model: $model, choices: $choices)';
   }
 
-@override
-bool operator ==(covariant ResponseModel other) {
-if (identical(this, other)) return true;
+  @override
+  bool operator ==(covariant ResponseModel other) {
+    if (identical(this, other)) return true;
 
-return other.id == id &&
+    return other.id == id &&
         other.object == object &&
         other.model == model &&
-listEquals(other.choices, choices);
+        listEquals(other.choices, choices);
   }
 
-@override
-int get hashCode {
-return id.hashCode ^ object.hashCode ^ model.hashCode ^ choices.hashCode;
+  @override
+  int get hashCode {
+    return id.hashCode ^ object.hashCode ^ model.hashCode ^ choices.hashCode;
   }
 }
